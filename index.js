eea722434ca18c2266990ed510f1845dd5f063b3
Guigo <rsbecker@inf.ufsm.br>
2024-03-17 11:27:13 -0300

    Implementar função para adicionar tarefas

---
+++ index.js
@@ -0,0 +1,26 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const inputBotao = document.querySelector('.input-botao');
+const tarefasLista = document.querySelector('.tarefas-lista');
+
+inputBotao.addEventListener('click', adicionarTarefa);
+
+function adicionarTarefa(e) {
+  const tarefa = document.createElement('div');
+  tarefa.classList.add('tarefa');
+
+  const tarefaItem = document.createElement('li');
+  tarefaItem.classList.add('tarefa-item');
+  tarefa.appendChild(tarefaItem);
+
+  const botaoConcluido = document.createElement('button');
+  botaoConcluido.innerHTML = '<i class="fas fa-check"></i>';
+  botaoConcluido.classList.add('botao-concluido');
+  tarefa.appendChild(botaoConcluido);
+
+  const botaoExcluir = document.createElement('button');
+  botaoExcluir.innerHTML = '<i class="fas fa-trash"></i>';
+  botaoExcluir.classList.add('botao-excluir');
+  tarefa.appendChild(botaoExcluir);
+
+  tarefasLista.appendChild(tarefa);
+}
